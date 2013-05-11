1c778bbce04630b842ec948d602f5503c7fc9dc2
chickenknife <chickenknife127@gmail.com>
2013-05-11 14:11:07 +0900

    create project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Configuration;
+namespace MediaImport
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        System.IO.DirectoryInfo destDir ;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            destDir = new DirectoryInfo(ConfigurationManager.AppSettings["destractDirectory"]);
+            this.textBox1.Text = destDir.FullName;
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            saveSettings();
+
+            //get drop info
+            string[] dirName =(string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName[0]);
+
+            if (stackPanel1.Children.Cast<driveLabel>().Where(x => x.driveinfo.RootDirectory.Name == dirInfo.Root.Name).ToArray().Length == 0)
+            {
+                driveLabel drivelbl = new driveLabel(dirInfo, destDir);
+                // drivelbl.Name = dirInfo.Root.Name.Replace(":\\","");
+
+                //label design
+                drivelbl.RenderSize = new Size(300, 60);
+                drivelbl.Margin = new Thickness(2, 4, 2, 4);
+                drivelbl.lblDriveLabel.Content = drivelbl.driveinfo.Name;
+
+                //add to stackpanel
+                this.stackPanel1.Children.Add(drivelbl);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog fb = new System.Windows.Forms.FolderBrowserDialog();
+
+            fb.SelectedPath = this.destDir.FullName;
+
+            if (fb.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox1.Text = fb.SelectedPath;
+            }
+        }
+
+        private void saveSettings()
+        {
+            //Create the object
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            //make changes
+            config.AppSettings.Settings["destractDirectory"].Value = this.textBox1.Text;
+
+            //save to apply changes
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+            destDir = new System.IO.DirectoryInfo(this.textBox1.Text);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            saveSettings();
+        }
+    }
+}
---
+++ driveLabel.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using System.Management;
+namespace MediaImport
+{
+    public partial class driveLabel : UserControl
+    {
+
+        #region constractor
+        public driveLabel()
+        {
+            InitializeComponent();
+            importDriveWatcher();
+        }
+
+        public DriveInfo driveinfo;
+        public DirectoryInfo destDirectory;
+        public driveLabel(DirectoryInfo _dirInfo,DirectoryInfo _destDirectory)
+        {
+            InitializeComponent(); 
+            driveinfo = new DriveInfo(_dirInfo.Root.Name);
+            this.lblDriveLabel.Content = driveinfo.Name;
+            this.destDirectory = _destDirectory;
+            importDriveWatcher();
+        }
+        #endregion
+
+        #region drivewatcher
+        public void importDriveWatcher()
+        {
+            try
+            {
+                WqlEventQuery q = new WqlEventQuery();
+                q.EventClassName = "__InstanceModificationEvent";
+                q.WithinInterval = new TimeSpan(0, 0, 1);
+                q.Condition = @"TargetInstance ISA 'Win32_LogicalDisk' and TargetInstance.DriveType = 5";
+
+                ConnectionOptions opt = new ConnectionOptions();
+                opt.EnablePrivileges = true;
+                opt.Authority = null;
+                opt.Authentication = AuthenticationLevel.Default;
+                //opt.Username = "Administrator";
+                //opt.Password = "";
+                ManagementScope scope = new ManagementScope("\\root\\CIMV2", opt);
+
+                ManagementEventWatcher watcher = new ManagementEventWatcher(scope, q);
+                watcher.EventArrived += new EventArrivedEventHandler(watcher_EventArrived);
+                watcher.Start();
+            }
+            catch (ManagementException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void watcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            //all drive check
+            ManagementBaseObject wmiDevice = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            string driveName = (string)wmiDevice["DeviceID"];
+            Console.WriteLine(driveName);
+            Console.WriteLine(wmiDevice.Properties["VolumeName"].Value);
+            Console.WriteLine((string)wmiDevice["Name"]);
+
+            //detect target drive
+            if (wmiDevice.Properties["VolumeName"].Value != null && this.driveinfo.Name.Replace("\\","") == driveName)
+            {
+                fastcopyStart(driveName,wmiDevice.Properties["VolumeName"].Value.ToString());
+            }
+            else
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region  fastcopy
+        private void fastcopyStart(string copyroot,string volumelabel)
+        {
+            lblWrite("fastcopy processing...");
+            DirectoryInfo destDir =
+                new DirectoryInfo(destDirectory + "\\" + System.DateTime.Now.ToString("yyyyMMdd") + "\\" + volumelabel);
+
+            if (destDir.Exists)
+            {
+                for (int i = 1; destDir.Exists; i++)
+                {
+                    destDir =
+                        new DirectoryInfo(destDirectory + "\\" + System.DateTime.Now.ToString("yyyyMMdd") + "\\" + volumelabel + "_" + i.ToString());
+                }
+            }
+            
+            Process proc = new Process();
+           
+            ProcessStartInfo procInfo = new ProcessStartInfo();
+            procInfo.CreateNoWindow = true;
+            procInfo.FileName = @"fastcopy\fastcopy.exe";
+            procInfo.Arguments =
+                string.Format(@"/auto_close /force_start /filelog /log {0} /to={1}"
+                , copyroot + "\\"
+                , "\"" + destDir.FullName + "\"");
+            proc.StartInfo = procInfo;
+
+            proc.Start();
+
+            proc.WaitForExit();
+            lblWrite("fastcopy success");
+        }
+        #endregion
+
+        #region form
+        delegate void formdelegate();
+
+        private void lblWrite(string text)
+        {
+            Dispatcher.Invoke((formdelegate)delegate()
+            {
+                this.lblDriveLabel.Content = string.Format("{0} [ {1} ] ({2})", driveinfo.Name + " " + driveinfo.VolumeLabel, text, System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+            });
+        }
+        #endregion
+    }
+}
